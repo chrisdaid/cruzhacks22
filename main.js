976d4815f76c1432910eaace927c49850318b61e
chrisdaid <c.chrisdb@gmail.com>
2022-01-15 01:17:50 -0500

    first main commit
    Co-authored-by: Fyez Dean <fdean@users.noreply.github.com>
    Co-authored-by: eharrison0 <eharrison0@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,33 @@
+const password = document.getElementById("password");
+const showPassword = document.getElementById("show-password");
+const hidePassword = document.getElementById("hide-password");
+//click eye to reveal password
+showPassword.addEventListener("click", () => {
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+});
+// click eye with slash to hide password
+hidePassword.addEventListener("click", () => {
+  const type = password.getAttribute("type") === "text" ? "password" : "text";
+  password.setAttribute("type", type);
+});
+
+const showPass = document.querySelector(".fa-eye");
+const hidePass = document.querySelector(".fa-eye-slash");
+// once eye is clicked, hide the eye and reveal the eye w slash
+showPass.addEventListener("click", () => {
+  showPass.classList.toggle("hidden");
+  hidePass.classList.toggle("hidden");
+});
+// once eye w slash is clicked, hide the eye w slash and reveal the eye
+hidePass.addEventListener("click", () => {
+  hidePass.classList.toggle("hidden");
+  showPass.classList.toggle("hidden");
+});
+
+// prevent form from submitting
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
